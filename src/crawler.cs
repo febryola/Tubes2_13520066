b31e93868181a8ccead4b2b4f5c0718650f1039f
febryola <febryola0402@gmail.com>
2022-03-03 05:30:56 +0700

    testing awal print

---
+++ src/crawler.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace tubes2_stima
+{
+    public class Graf
+    {
+        public void printInfo()
+        {
+            Console.Write("Moga kelarrrr\n");
+        }
+    }
+}
---
+++ src/program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace tubes2_stima
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Console.Write("Moga kelarrrr\n");
+        }
+    }
+    
+}
