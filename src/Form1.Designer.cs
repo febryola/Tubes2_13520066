cf6bb6ddb4cf33057a0a089587ea494c690b82bd
putriliza <putriliza05@gmail.com>
2022-03-03 23:54:31 +0700

    test GUI

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace src
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.title = new System.Windows.Forms.Label();
+            this.labelInput = new System.Windows.Forms.Label();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.labelChooseDir = new System.Windows.Forms.Label();
+            this.labelInputFile = new System.Windows.Forms.Label();
+            this.textBoxFilename = new System.Windows.Forms.TextBox();
+            this.checkBoxAllOcc = new System.Windows.Forms.CheckBox();
+            this.labelMethod = new System.Windows.Forms.Label();
+            this.radioButtonBFS = new System.Windows.Forms.RadioButton();
+            this.radioButtonDFS = new System.Windows.Forms.RadioButton();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.title.AutoSize = true;
+            this.title.BackColor = System.Drawing.Color.White;
+            this.title.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.title.ForeColor = System.Drawing.Color.DarkCyan;
+            this.title.Location = new System.Drawing.Point(343, 29);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(306, 54);
+            this.title.TabIndex = 11;
+            this.title.Text = "WhyNotSearch";
+            this.title.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // labelInput
+            // 
+            this.labelInput.AutoSize = true;
+            this.labelInput.BackColor = System.Drawing.Color.White;
+            this.labelInput.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelInput.ForeColor = System.Drawing.Color.DarkCyan;
+            this.labelInput.Location = new System.Drawing.Point(51, 121);
+            this.labelInput.Name = "labelInput";
+            this.labelInput.Size = new System.Drawing.Size(72, 32);
+            this.labelInput.TabIndex = 12;
+            this.labelInput.Text = "Input";
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.BackColor = System.Drawing.Color.White;
+            this.labelOutput.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelOutput.ForeColor = System.Drawing.Color.DarkCyan;
+            this.labelOutput.Location = new System.Drawing.Point(423, 121);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(92, 32);
+            this.labelOutput.TabIndex = 13;
+            this.labelOutput.Text = "Output";
+            // 
+            // labelChooseDir
+            // 
+            this.labelChooseDir.AutoSize = true;
+            this.labelChooseDir.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelChooseDir.Location = new System.Drawing.Point(51, 165);
+            this.labelChooseDir.Name = "labelChooseDir";
+            this.labelChooseDir.Size = new System.Drawing.Size(228, 25);
+            this.labelChooseDir.TabIndex = 14;
+            this.labelChooseDir.Text = "Choose Starting Directory";
+            // 
+            // labelInputFile
+            // 
+            this.labelInputFile.AutoSize = true;
+            this.labelInputFile.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelInputFile.Location = new System.Drawing.Point(51, 231);
+            this.labelInputFile.Name = "labelInputFile";
+            this.labelInputFile.Size = new System.Drawing.Size(145, 25);
+            this.labelInputFile.TabIndex = 15;
+            this.labelInputFile.Text = "Input File Name";
+            // 
+            // textBoxFilename
+            // 
+            this.textBoxFilename.Location = new System.Drawing.Point(51, 259);
+            this.textBoxFilename.Name = "textBoxFilename";
+            this.textBoxFilename.Size = new System.Drawing.Size(290, 31);
+            this.textBoxFilename.TabIndex = 16;
+            // 
+            // checkBoxAllOcc
+            // 
+            this.checkBoxAllOcc.AutoSize = true;
+            this.checkBoxAllOcc.Location = new System.Drawing.Point(51, 296);
+            this.checkBoxAllOcc.Name = "checkBoxAllOcc";
+            this.checkBoxAllOcc.Size = new System.Drawing.Size(184, 29);
+            this.checkBoxAllOcc.TabIndex = 17;
+            this.checkBoxAllOcc.Text = "Find All Occurence";
+            this.checkBoxAllOcc.UseVisualStyleBackColor = true;
+            // 
+            // labelMethod
+            // 
+            this.labelMethod.AutoSize = true;
+            this.labelMethod.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelMethod.Location = new System.Drawing.Point(51, 358);
+            this.labelMethod.Name = "labelMethod";
+            this.labelMethod.Size = new System.Drawing.Size(137, 25);
+            this.labelMethod.TabIndex = 18;
+            this.labelMethod.Text = "Search Method";
+            // 
+            // radioButtonBFS
+            // 
+            this.radioButtonBFS.AutoSize = true;
+            this.radioButtonBFS.Location = new System.Drawing.Point(51, 386);
+            this.radioButtonBFS.Name = "radioButtonBFS";
+            this.radioButtonBFS.Size = new System.Drawing.Size(66, 29);
+            this.radioButtonBFS.TabIndex = 19;
+            this.radioButtonBFS.TabStop = true;
+            this.radioButtonBFS.Text = "BFS";
+            this.radioButtonBFS.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonDFS
+            // 
+            this.radioButtonDFS.AutoSize = true;
+            this.radioButtonDFS.Location = new System.Drawing.Point(51, 421);
+            this.radioButtonDFS.Name = "radioButtonDFS";
+            this.radioButtonDFS.Size = new System.Drawing.Size(69, 29);
+            this.radioButtonDFS.TabIndex = 20;
+            this.radioButtonDFS.TabStop = true;
+            this.radioButtonDFS.Text = "DFS";
+            this.radioButtonDFS.UseVisualStyleBackColor = true;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.BackColor = System.Drawing.Color.RoyalBlue;
+            this.buttonSearch.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonSearch.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonSearch.ForeColor = System.Drawing.Color.White;
+            this.buttonSearch.Location = new System.Drawing.Point(51, 475);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(112, 34);
+            this.buttonSearch.TabIndex = 21;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1014, 592);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.radioButtonDFS);
+            this.Controls.Add(this.radioButtonBFS);
+            this.Controls.Add(this.labelMethod);
+            this.Controls.Add(this.checkBoxAllOcc);
+            this.Controls.Add(this.textBoxFilename);
+            this.Controls.Add(this.labelInputFile);
+            this.Controls.Add(this.labelChooseDir);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.labelInput);
+            this.Controls.Add(this.title);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private FolderBrowserDialog folderBrowserDialog1;
+        private Label title;
+        private Label labelInput;
+        private Label labelOutput;
+        private Label labelChooseDir;
+        private Label labelInputFile;
+        private TextBox textBoxFilename;
+        private CheckBox checkBoxAllOcc;
+        private Label labelMethod;
+        private RadioButton radioButtonBFS;
+        private RadioButton radioButtonDFS;
+        private Button buttonSearch;
+    }
+}
--- src/program.cs
+++ src/program.cs
@@ -1,21 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-
-namespace tubes2_stima
+namespace src
 {
-    static class Program
+    internal static class Program
     {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
         static void Main()
         {
-            Console.Write("Moga kelarrrr\n");
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
         }
     }
-    
 }
